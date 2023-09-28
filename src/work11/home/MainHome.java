efdb5626f7a650f8376263ce49627c20fba9cc68
Angelina <n.angelina169@gmail.com>
2023-09-28 21:12:23 +0300

    work11

--- src/work11/home/MainHome.java
+++ src/work11/home/MainHome.java
@@ -3,23 +3,10 @@ package work11.home;
 public class MainHome {
     public static void main(String[] args) {
         System.out.println("task1");
-        Registration.registerInSystem("alise34","wer4","wer94");
+        Registration.registerInSystem("alise34","wer","wer");
         System.out.println("task*");
-        getFinallyDoNotWork(0);
+      //  getFinallyDoNotWork(0);
     }
 
-    private static void getFinallyDoNotWork(int k) {
-        try{
-            System.out.println("5/"+k+" Whole part: "+5/k+" Remainder of the division: "+5%k);
-            System.exit(0);
-        }
-        catch (ArithmeticException e)
-        {
-            System.out.println("Division by zero");
-            System.exit(0);
-        }
-        finally {
-            System.out.println("Finally work");
-        }
-    }
+
 }
