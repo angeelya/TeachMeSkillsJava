28955f6f6bcbb286c6ea5e393e51f0c2ea8f578e
Angelina <n.angelina169@gmail.com>
2024-01-04 20:07:44 +0300

    work31

--- src/work31/home/facade/BoilingSensor.java
+++ src/work31/home/facade/BoilingSensor.java
@@ -3,27 +3,28 @@ package work31.home.facade;
 import java.util.Random;
 
 public class BoilingSensor {
-   private Integer actualTemperature, setTemperature;
+    private Integer actualTemperature, setTemperature;
 
     public BoilingSensor(Integer setTemperature) {
         this.actualTemperature = getActualTemperature();
         this.setTemperature = setTemperature;
-        System.out.println("Actual temperature: "+actualTemperature+"\nSet temperature: "+setTemperature);
+        System.out.println("Actual temperature: " + actualTemperature + "\nSet temperature: " + setTemperature);
     }
 
     private Integer getActualTemperature() {
         Random random = new Random();
         return random.nextInt(101);
     }
-    public void increaseTemperature()
-    {
+
+    public void increaseTemperature() {
         actualTemperature++;
     }
-    public boolean compareActualAndSetTemperature()
-    {
-        if(actualTemperature<=setTemperature) return true;
+
+    public boolean compareActualAndSetTemperature() {
+        if (actualTemperature <= setTemperature) return true;
         else return false;
     }
+
     public boolean checkTemperature() {
         if (actualTemperature == setTemperature) return true;
         else return false;
